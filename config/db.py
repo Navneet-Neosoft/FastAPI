ff2ece3a4980a64754158a82cc6ecc92c6dab0ce
Navneet-Neosoft <navneet.chourasia.neosoftmail.com>
2021-07-28 19:03:56 +0530

    initial commit

---
+++ config/db.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine,MetaData
+
+engine = create_engine("mysql+pymysql://root@localhost:3306/testdb")
+meta = MetaData()
+conn = engine.connect()
---
+++ models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Table,Column
+from sqlalchemy.sql.sqltypes import String,Integer
+from config.db import meta
+
+users = Table(
+    'users',meta,
+    Column('id',Integer,primary_key=True),
+    Column('name',String(255)),
+    Column('email',String(255)),
+    Column('password',String(255))
+)
